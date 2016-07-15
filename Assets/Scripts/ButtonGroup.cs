ff63005ec29f7a4c4dc9e5be32d18424509317d9
yearown <463383934@qq.com>
2016-07-15 16:16:34 +0800

    123
    hao

---
+++ Assets/Scripts/ButtonGroup.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ButtonGroup : MonoBehaviour {
+
+    public List<Button> btnGroup;
+    public SpriteRenderer selectedImage;
+    public SpriteRenderer normalImage;
+	// Use this for initialization
+	void Start () {
+        btnGroup = new List<Button>();
+        GameObject selected = GameObject.Find("ButtonAcceleratorOverSprite");
+        selectedImage = selected.GetComponent<SpriteRenderer>();
+
+        GameObject normal = GameObject.Find("ButtonArrowUpSprite");
+        normalImage = normal.GetComponent<SpriteRenderer>();
+
+        for(int i = 0;i<5;i++)
+        {
+            GameObject item = GameObject.Find("selectItem"+i);
+            Button btn = item.GetComponent<Button>();
+            btn.onClick.AddListener(delegate()
+            {
+                this.btnClick(item);
+            });
+            btnGroup.Add(btn);
+        }
+	}
+
+    public void btnClick(GameObject obj)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Text text = btnGroup[i].GetComponentInChildren<Text>();
+            text.fontSize = 12;
+            text.color = Color.black;
+            btnGroup[i].image.sprite = normalImage.sprite;
+        }
+
+        int currentSelectIndex = 0;
+        switch(obj.name)
+        {
+            case "selectItem0":
+                currentSelectIndex = 0;
+                break;
+            case "selectItem1":
+                currentSelectIndex = 1;
+                break;
+            case "selectItem2":
+                currentSelectIndex = 2;
+                break;
+            case "selectItem3":
+                currentSelectIndex = 3;
+                break;
+            case "selectItem4":
+                currentSelectIndex = 4;
+                break;
+        }
+
+        btnGroup[currentSelectIndex].image.sprite = selectedImage.sprite;
+        Text btnText = btnGroup[currentSelectIndex].GetComponentInChildren<Text>();
+        btnText.fontSize = 20;        
+        btnText.color = Color.red;
+    }
+
+    public void setSelectedItem(int index)
+    {
+        for(int i=0;i<5;i++)
+        {
+            btnGroup[i].image.sprite = normalImage.sprite;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Page1.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Page1 : MonoBehaviour {
+
+    public List<Button> btnGroup;
+    // Use this for initialization
+    void Start () {
+        btnGroup = new List<Button>();
+        for (int i = 1; i < 6; i++)
+        {
+            GameObject item1 = GameObject.Find("color2_" + i);
+            Image image1 = item1.GetComponent<Image>();
+            image1.enabled = false;
+        }
+
+        for (int i = 1; i < 6; i++)
+        {
+            GameObject item = GameObject.Find("colorBtn" + i);
+            Button btn = item.GetComponent<Button>();
+            btn.onClick.AddListener(delegate ()
+            {
+                this.btnClick(item);
+            });
+            btnGroup.Add(btn);
+        }
+
+
+        GameObject Back3d = GameObject.Find("3dBack");
+        Image back = Back3d.GetComponent<Image>();
+        back.enabled = false;
+
+        GameObject showBaiSe = GameObject.Find("showBaiSe");
+        Image showBaiSeImg = showBaiSe.GetComponent<Image>();
+        showBaiSeImg.enabled = false;
+
+        GameObject btn3 = GameObject.Find("btn360");
+        Button btnObj = btn3.GetComponent<Button>();
+        btnObj.onClick.AddListener(delegate ()
+        {
+            GameObject item1 = GameObject.Find("showBaiSe");
+            Image image1 = item1.GetComponent<Image>();
+            image1.enabled = true;
+            back.enabled = true;
+        });
+    }
+
+    public void btnClick(GameObject obj)
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            //Text text = btnGroup[i].GetComponentInChildren<Text>();
+            //text.fontSize = 12;
+            //text.color = Color.black;
+            //btnGroup[i].image.sprite = normalImage.sprite;
+            
+            GameObject item0 = GameObject.Find("color2_" + i);
+            Image image0 = item0.GetComponent<Image>();
+            image0.enabled = false;
+        }
+        
+        int currentSelectIndex = 1;
+        switch (obj.name)
+        {
+            case "colorBtn1":
+                currentSelectIndex = 1;
+                break;
+            case "colorBtn2":
+                currentSelectIndex = 2;
+                break;
+            case "colorBtn3":
+                currentSelectIndex = 3;
+                break;
+            case "colorBtn4":
+                currentSelectIndex = 4;
+                break;
+            case "colorBtn5":
+                currentSelectIndex = 5;
+                break;
+        }
+
+        GameObject item = GameObject.Find("color2_" + currentSelectIndex);
+        Image image1 = item.GetComponent<Image>();
+        image1.enabled = true;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
